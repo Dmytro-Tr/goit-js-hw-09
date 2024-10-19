51641282277985e9af54b7fa770a91ca0b1e51df
Dmtro_Tr <Di4gamegem@gmail.com>
2024-10-19 22:10:55 +0300

    2-form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,41 @@
-console.log('Form');
+let formData = {
+  email: '',
+  message: '',
+};
+
+const feedbackForm = document.querySelector('.feedback-form');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+populateTextAres();
+
+feedbackForm.addEventListener('input', handleInput);
+feedbackForm.addEventListener('submit', handleSubmit);
+
+function handleInput(event) {
+  formData[event.target.name] = event.target.value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+    return;
+  }
+  console.log(formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+  formData = { email: '', message: '' };
+  feedbackForm.reset();
+}
+
+function populateTextAres() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  if (savedData) {
+    formData = JSON.parse(savedData);
+    feedbackForm.elements.email.value = savedData.email || '';
+    feedbackForm.elements.message.value = savedData.message || '';
+  }
+}
